7a3f028cab336267add4ceac2fab4aec454bda73
Mrigank Gupta <mrigankgupta@gmail.com>
2018-11-26 20:48:24 +0530

    Initial Commit

---
+++ MGPicsque/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MGPicsque
+//
+//  Created by Gupta, Mrigank on 26/11/18.
+//  Copyright © 2018 Gupta, Mrigank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
